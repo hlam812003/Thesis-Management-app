359897207383c82a4c2f4f061408ba87649b03b2
Cevan <110558291+Cevan23@users.noreply.github.com>
2023-11-18 17:08:32 +0700

    back-end connect mongodb and add university 18/11 commit

---
+++ backend/routes/university.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const router = express.Router();
+
+const universityController = require('../controllers/university');
+// const checkAuth = require('../middleware/check-auth');
+
+
+//URL: /university
+
+router.get("/", universityController.get_all);    //get all universities with pagination
+router.get("/noPages",universityController.get_all_noPages) //get all , no pagination
+router.get("/:universityId", universityController.get_byId) //get university by id
+
+//router.delete("/:userId", checkAuth, thesisController.user_delete); // 
+//search thesis
+module.exports = router;
