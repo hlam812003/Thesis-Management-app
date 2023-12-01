d2a222445a50bbbcf08ffa76d11ad5d8a9316458
HWAN <68631787+hlam812003@users.noreply.github.com>
2023-12-01 15:28:04 +0700

    Update Login, Signup Page

--- frontend/pages/login.vue
+++ frontend/pages/login.vue
@@ -1,18 +1,40 @@
 <template>
     <Body>
         <section class="w-full h-screen flex items-center overflow-hidden">
-            <div class="w-[62%] h-screen p-10">
-                <div class="w-full flex items-center justify-between">
+            <div class="test w-[62%] h-screen p-10 bg-[#1C1C1E]">
+                <div 
+                class="w-full flex items-center justify-between"
+                v-motion
+                :initial="{ opacity: 0 }"
+                :enter="{ opacity: 1, scale: 1 }"
+                :delay="100"
+                >
                     <ULink to="/" class="flex items-center gap-1">
-                        <UIcon name="i-heroicons-chevron-left" class="text-[1.2rem]"/>
+                        <UIcon name="i-heroicons-chevron-left" class="text-[1.4rem]"/>
                         <p class="font-[Roboto] text-[1rem] font-light capitalize">quay về</p>
                     </ULink>
                     <p class="font-[Roboto] text-[1rem] font-light">Bạn chưa có tài khoản? <ULink to="/signup" class="font-bold uppercase transition-all hover:text-[rgb(0,220,130)]">tạo ngay</ULink></p>
                 </div>
-                <div class="w-full h-full flex items-center justify-center">
-                    <div class="w-[80%] h-[80%] flex items-center justify-center flex-col">
-                        <h2 class="text-[1.5rem] font-bold font-[Roboto] uppercase mb-[1.1rem] mt-[-1.6rem]">Chào mừng đã quay trở lại</h2>
-                        <p class="text-[1rem] font-[Roboto] font-light uppercase mb-[-7rem]">đăng nhập để tiếp tục</p>
+                <div class="w-full h-full flex items-center justify-center"
+                v-motion
+                :initial="{ opacity: 0 }"
+                :enter="{ opacity: 1, scale: 1 }"
+                :delay="300"
+                >
+                    <div class="w-[80%] h-[80%] flex items-center justify-center flex-col mt-[1.2rem]">
+                        <h2 class="text-[1.5rem] font-bold font-[Roboto] uppercase mb-[1.1rem] mt-[-.6rem]">Chào mừng đã quay trở lại</h2>
+                        <p class="text-[1rem] font-[Roboto] font-light uppercase mb-[2rem]">đăng nhập để tiếp tục</p>
+                        <div class="w-full flex items-center justify-center relative mb-[2rem]">
+                            <button class="login__google--btn">
+                                <Icon icon="devicon:google"/>
+                                <span class="font-[Roboto] font-medium text-[.9rem] transition-all">Đăng nhập với Google</span>
+                            </button>
+                        </div>
+                        <div class="w-[26rem] flex items-center justify-center gap-2 mb-[-6rem]">
+                            <span class="w-full h-[.05rem] bg-[#ffffffc0]"></span>
+                            <span class="w-full font-[Roboto] text-center font-light text-[.85rem]">Hoặc đăng nhập với</span>
+                            <span class="w-full h-[.05rem] bg-[#ffffffc0]"></span>
+                        </div>
                         <UForm class="w-full h-full flex items-center justify-center flex-col gap-[2.5rem] mb-[-5.5rem]" action="">
                             <UFormGroup label="Email của bạn" name="email" class="w-[26rem] font-[Roboto] text-[1rem] relative">
                                 <UIcon name="i-heroicons-envelope-open-solid" class="absolute top-[.85rem] left-3 text-[1.1rem]"/>
@@ -20,7 +42,8 @@
                             </UFormGroup>
                             <UFormGroup label="Mật khẩu" name="password" class="w-[26rem] font-[Roboto] text-[1rem] relative">
                                 <UIcon name="i-heroicons-lock-closed-solid" class="absolute top-[.85rem] left-3 text-[1.1rem]"/>
-                                <input type="password" class="w-full h-[3rem] outline-none pl-10 pr-4 bg-transparent border-b-[1px] border-b-[#ffffffc0] transition-all focus:border-b-[rgb(0,220,130)] text-[1.05rem] font-light" required autocomplete="off">
+                                <input :type="isPasswordVisible ? 'text' : 'password'" class="w-full h-[3rem] outline-none pl-10 pr-11 bg-transparent border-b-[1px] border-b-[#ffffffc0] transition-all focus:border-b-[rgb(0,220,130)] text-[1.05rem] font-light" required autocomplete="off">
+                                <UIcon :name="isPasswordVisible ? 'i-heroicons-eye-solid' : 'i-heroicons-eye-slash-solid'" class="absolute top-[.95rem] right-4 cursor-pointer" @click="togglePasswordVisibility"/>
                             </UFormGroup>
                             <div class="w-[26rem] flex items-center justify-between">
                                 <div class="flex items-center">
@@ -41,19 +64,40 @@
                     </div>
                 </div>
             </div>
-            <div class="w-[38%] h-screen">
-              <NuxtImg src="/photo2.jpg" class="w-full h-full"/>
+            <div class="w-[38%] h-screen"
+            v-motion
+            :initial="{ opacity: 0 }"
+            :enter="{ opacity: 1, scale: 1 }"
+            :delay="10"
+            >
+              <NuxtImg src="/photo2.jpg" class="w-full h-full border-l-[.2rem] border-l-[white]" />
             </div>
         </section>
     </Body>
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue';
+import { Icon } from '@iconify/vue';
+
+const isPasswordVisible = ref(false);
+
+function togglePasswordVisibility() {
+  isPasswordVisible.value = !isPasswordVisible.value;
+}
+
 useHead({
     title: 'Đăng Nhập',
 })
 </script>
 
-<style scoped>
-  
+<style scoped lang="postcss">
+
+.login__google--btn {
+    @apply  flex items-center justify-center gap-4 w-[26rem] h-[3.25rem] outline-none border-[.1rem] rounded-full transition-all hover:border-[rgb(0,220,130)];
+    &:hover span {
+        @apply  text-[rgb(0,220,130)];
+    }
+}
+
 </style>
--- frontend/pages/signup.vue
+++ frontend/pages/signup.vue
@@ -1,32 +1,59 @@
 <template>
     <Body>
         <section class="w-full h-screen flex items-center overflow-hidden">
-            <div class="w-[38%] h-screen">
-                <NuxtImg src="/photo3.jpg" class="w-full h-full"/>
+            <div class="w-[38%] h-screen"
+            v-motion
+            :initial="{ opacity: 0 }"
+            :enter="{ opacity: 1, scale: 1 }"
+            :delay="10"
+            >
+                <NuxtImg src="/photo3.jpg" class="w-full h-full border-r-[.2rem] border-r-[white]"/>
             </div>
-            <div class="w-[62%] h-screen p-10">
-                <div class="w-full flex items-center justify-between">
+            <div class="w-[62%] h-screen p-10 bg-[#1C1C1E]">
+                <div class="w-full flex items-center justify-between"
+                v-motion
+                :initial="{ opacity: 0 }"
+                :enter="{ opacity: 1, scale: 1 }"
+                :delay="100"
+                >
                     <ULink to="/login" class="flex items-center gap-1">
-                        <UIcon name="i-heroicons-chevron-left" class="text-[1.2rem]"/>
+                        <UIcon name="i-heroicons-chevron-left" class="text-[1.4rem]"/>
                         <p class="font-[Roboto] text-[1rem] font-light capitalize">quay về đăng kí</p>
                     </ULink>
                 </div>
-                <div class="w-full h-full flex items-center justify-center">
+                <div class="w-full h-full flex items-center justify-center"
+                v-motion
+                :initial="{ opacity: 0 }"
+                :enter="{ opacity: 1, scale: 1 }"
+                :delay="300"
+                >
                     <div class="w-[80%] h-[80%] flex items-center justify-center flex-col">
-                        <h2 class="text-[1.5rem] font-bold font-[Roboto] uppercase mb-[1.1rem] mt-[-1.6rem]">trở thành thành viên của chúng tôi</h2>
-                        <p class="text-[1rem] font-[Roboto] font-light uppercase mb-[-3.5rem]">đăng kí ngay để kết nối, chia sẻ, và phát triển cùng đồ án của bạn</p>
+                        <h2 class="text-[1.5rem] font-bold font-[Roboto] uppercase mb-[1.1rem] mt-[-3rem]">trở thành thành viên của chúng tôi</h2>
+                        <p class="text-[1rem] font-[Roboto] font-light uppercase mb-[-.1rem]">đăng kí ngay để kết nối, chia sẻ, và phát triển cùng đồ án của bạn</p>
                         <UForm class="w-full h-full flex items-center justify-center flex-col gap-[2.5rem] mb-[-5.5rem]" action="">
-                            <UFormGroup label="Username" name="username" class="w-[26rem] font-[Roboto] text-[1rem] relative">
-                                <UIcon name="i-heroicons-user-solid" class="absolute top-[.85rem] left-3 text-[1.1rem]"/>
-                                <input type="text" class="w-full h-[3rem] outline-none pl-10 pr-4 bg-transparent border-b-[1px] border-b-[#ffffffc0] transition-all focus:border-b-[rgb(0,220,130)] text-[1.05rem] font-light" required autocomplete="off">
-                            </UFormGroup>
+                            <div class="w-[26rem] flex items-center justify-between gap-12">
+                                <UFormGroup label="Họ của bạn" name="username" class="w-1/2 font-[Roboto] text-[1rem] relative">
+                                    <UIcon name="i-heroicons-user-solid" class="absolute top-[.85rem] left-3 text-[1.1rem]"/>
+                                    <input type="text" class="w-full h-[3rem] outline-none pl-10 pr-4 bg-transparent border-b-[1px] border-b-[#ffffffc0] transition-all focus:border-b-[rgb(0,220,130)] text-[1.05rem] font-light" required autocomplete="off">
+                                </UFormGroup>
+                                <UFormGroup label="Tên của bạn" name="username" class="w-1/2 font-[Roboto] text-[1rem] relative">
+                                    <UIcon name="i-heroicons-user-solid" class="absolute top-[.85rem] left-3 text-[1.1rem]"/>
+                                    <input type="text" class="w-full h-[3rem] outline-none pl-10 pr-4 bg-transparent border-b-[1px] border-b-[#ffffffc0] transition-all focus:border-b-[rgb(0,220,130)] text-[1.05rem] font-light" required autocomplete="off">
+                                </UFormGroup>
+                            </div>
                             <UFormGroup label="Email đăng kí" name="email" class="w-[26rem] font-[Roboto] text-[1rem] relative">
                                 <UIcon name="i-heroicons-envelope-open-solid" class="absolute top-[.85rem] left-3 text-[1.1rem]"/>
                                 <input type="email" class="w-full h-[3rem] outline-none pl-10 pr-4 bg-transparent border-b-[1px] border-b-[#ffffffc0] transition-all focus:border-b-[rgb(0,220,130)] text-[1.05rem] font-light" required autocomplete="off">
                             </UFormGroup>
                             <UFormGroup label="Mật khẩu" name="password" class="w-[26rem] font-[Roboto] text-[1rem] relative">
                                 <UIcon name="i-heroicons-lock-closed-solid" class="absolute top-[.85rem] left-3 text-[1.1rem]"/>
-                                <input type="password" class="w-full h-[3rem] outline-none pl-10 pr-4 bg-transparent border-b-[1px] border-b-[#ffffffc0] transition-all focus:border-b-[rgb(0,220,130)] text-[1.05rem] font-light" required autocomplete="off">
+                                <input :type="passwordVisibility.password ? 'text' : 'password'" class="w-full h-[3rem] outline-none pl-10 pr-11 bg-transparent border-b-[1px] border-b-[#ffffffc0] transition-all focus:border-b-[rgb(0,220,130)] text-[1.05rem] font-light" required autocomplete="off">
+                                <UIcon :name="passwordVisibility.password ? 'i-heroicons-eye-solid' : 'i-heroicons-eye-slash-solid'" class="absolute top-[.95rem] right-4 cursor-pointer" @click="togglePasswordVisibility('password')"/>
+                            </UFormGroup>
+                            <UFormGroup label="Xác nhận mật khẩu" name="confirmPassword" class="w-[26rem] font-[Roboto] text-[1rem] relative">
+                                <UIcon name="i-heroicons-lock-closed-solid" class="absolute top-[.85rem] left-3 text-[1.1rem]"/>
+                                <input :type="passwordVisibility.confirmPassword ? 'text' : 'password'" class="w-full h-[3rem] outline-none pl-10 pr-11 bg-transparent border-b-[1px] border-b-[#ffffffc0] transition-all focus:border-b-[rgb(0,220,130)] text-[1.05rem] font-light" required autocomplete="off">
+                                <UIcon :name="passwordVisibility.confirmPassword ? 'i-heroicons-eye-solid' : 'i-heroicons-eye-slash-solid'" class="absolute top-[.95rem] right-4 cursor-pointer" @click="togglePasswordVisibility('confirmPassword')"/>
                             </UFormGroup>
                             <div class="w-[26rem] flex items-center justify-between">
                                 <div class="flex items-center">
@@ -38,7 +65,7 @@
                                     size="xl"
                                     variant="solid"
                                     :ui="{ rounded: 'rounded-full' }"
-                                    to="/">
+                                    to="/login">
                                     <span class="font-['Roboto'] uppercase text-[.9rem] font-bold">đăng ký</span>
                                 </UButton>
                             </div>
@@ -51,6 +78,16 @@
 </template>
 
 <script setup>
+import { reactive } from 'vue';
+
+const passwordVisibility = reactive({
+  password: false,
+  confirmPassword: false
+});
+
+function togglePasswordVisibility(field) {
+  passwordVisibility[field] = !passwordVisibility[field];
+}
 
 useHead({
     title: 'Đăng Ký',
