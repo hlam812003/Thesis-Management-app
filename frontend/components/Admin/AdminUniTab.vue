6e63b62826877f092b126b9f154d6581e62855d7
Cevan <110558291+Cevan23@users.noreply.github.com>
2023-12-18 20:24:02 +0700

    add file image buffer

--- frontend/components/Admin/AdminUniTab.vue
+++ frontend/components/Admin/AdminUniTab.vue
@@ -1,7 +1,34 @@
 <template>
-    <div class="w-3/4 h-screen p-4">
-      <div class="mt-4">
-        <h2>Update Area univer </h2>
-      </div>
-    </div>
+        <div>
+          <img :src="imageSrc" alt="Thesis Image" v-if="imageSrc" />
+        </div>
 </template>
+      
+<script setup>
+      import { ref, onMounted } from 'vue';
+      import adminService from '~/services/AdminService';
+      
+      const imageSrc = ref('');
+      
+      const showImg = async () => {
+        try {
+          const result = await adminService.Test();
+          if (result && result.images && result.images.data) {
+            const base64Image = arrayBufferToBase64(result.images.data);
+            imageSrc.value = `data:image/png;base64,${base64Image}`;
+          }
+        } catch (error) {
+          console.error(error);
+        }
+      };
+      
+      onMounted(() => {
+        showImg();
+      });
+      
+      function arrayBufferToBase64(arrayBuffer) {
+        const binary = String.fromCharCode.apply(null, new Uint8Array(arrayBuffer));
+        return btoa(binary);
+      }
+</script>
+      
